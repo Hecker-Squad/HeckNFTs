bfe8aa99522ce1f1fc43a6dadf2f874d0e5bd58f
Skyhero-admin <smnouman007@gmail.com>
2022-09-15 18:31:45 +0530

    feat: create-react-app done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hecking the world</h1>
+      <img
+        alt="cat hecking the world"
+        src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/309/hacker-cat_1f431-200d-1f4bb.png"
+      ></img>
+    </div>
+  );
+}
+
+export default App;
