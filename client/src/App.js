63c04e5a3ce48c54f71761048d0b1b5efd1cd1bc
Abhishek <abxisxekpanda@gmail.com>
2022-10-23 02:09:57 +0530

    Minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import Prodinfo from "./Components/Prodinfo.js";
 import Navbar from "./Components/Navbar.js";
+import Footer from "./Components/Footer.js";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Prodinfo />
+      <Footer />
     </div>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,143 +1,59 @@
+import logo from "./Assets/heck-logo.svg";
+
 const Navbar = () => {
   return (
-    <div className="navbar bg-neutral">
+    <div className="navbar bg-white">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">Heck NFT's</a>
+        <a className="btn btn-ghost normal-case text-xl text-black" href="./Assets/heck0logo.svg">
+          <img src={logo} alt="logo" id="logo" style={{ height: "30px", width: "30px" }} />
+          Heck NFT's</a>
         <div className="form-control">
           <input
             type="text"
             placeholder="Search"
-            className="input input-bordered"
+            className="input input-bordered bg-gray-200 focus:hover:bg-gray-300" style={{ width: "1200px" }}
           />
         </div>
       </div>
       <div className="flex-none gap-2">
-        <div className="dropdown dropdown-end">
-          <ul
-            tabIndex={0}
-            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a className="justify-between">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
+        <div className="navbar">
+          <div className="navbar-start">
+          </div>
+          <div className="navbar-center hidden lg:flex bg-white">
+            <ul className="menu menu-horizontal pr-10 text-black">
+              <li>
+                <a href="landing.html">Home</a>
+              </li>
+
+              <li>
+                <a href="collection.html">Collection</a>
+              </li>
+
+              <li>
+                <a href="forum.html">Forum</a>
+              </li>
+
+              <li>
+                <a href="support.html">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src="https://placeimg.com/80/80/people" alt="avatars" />
+                </div>
+              </label>
+              <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
                 <li>
-                  <a>Submenu 1</a>
+                  <a className="justify-between" href="profile.js">
+                    Profile
+                  </a>
                 </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
+                <li><a href="favourites.js">Favourites</a></li>
+                <li><a href="watchlist.js">Watch List</a></li>
               </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-          <div className="navbar bg-base-100 bg-neutral">
-            <div className="navbar-start">
-              <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h8m-8 6h16"
-                    />
-                  </svg>
-                </label>
-                <ul
-                  tabIndex={0}
-                  className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-                >
-                  <li>
-                    <a>Item 1</a>
-                  </li>
-                  <li tabIndex={0}>
-                    <a className="justify-between">
-                      Parent
-                      <svg
-                        className="fill-current"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                      </svg>
-                    </a>
-                    <ul className="p-2">
-                      <li>
-                        <a>Submenu 1</a>
-                      </li>
-                      <li>
-                        <a>Submenu 2</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a>Item 3</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="navbar-center hidden lg:flex bg-neutral">
-              <ul className="menu menu-horizontal pr-10 ">
-                <li>
-                  <a>Home</a>
-                </li>
-
-                <li>
-                  <a>Collection</a>
-                </li>
-
-                <li>
-                  <a>Blog</a>
-                </li>
-
-                <li>
-                  <a>Contact Us</a>
-                </li>
-              </ul>
-            </div>
-            <div className="navbar-end pl-5">
-              <a className="btn bg-primary text-accent-content">Connect Wallet</a>
             </div>
           </div>
         </div>
