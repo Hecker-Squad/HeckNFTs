41c8fbe62c48c35f39a5c928e6b3b164ab94eb4c
Abhishek <abxisxekpanda@gmail.com>
2022-10-27 20:28:47 +0530

    New tingz

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import Prodinfo from "./Components/Prodinfo.js";
+//import Prodinfo from "./Components/Prodinfo.js";
 import Navbar from "./Components/Navbar.js";
 import Footer from "./Components/Footer.js";
+import Main from "./Components/Main.js";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Prodinfo />
+      <Main />
       <Footer />
     </div>
   );
